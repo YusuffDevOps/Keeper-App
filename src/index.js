cfd7ad93fb1ce12b6c8231f1f87b905b75bcbcd0
Yusuff Kazeem <yusuffkazeem56@gmail.com>
2023-05-15 20:00:09 -0300

    Fixed npm run build errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import App from "./components/App";
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); 
-root.render(<App tab="home" />);
 
-
+//Render APP component
+ReactDOM.render(<App />, document.getElementById("root"));
 
