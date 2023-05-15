cc6a03c9c951306fa2fa4441e33946e6a345ab22
Yusuff Kazeem <yusuffkazeem56@gmail.com>
2023-05-15 20:48:16 -0300

    Fixed npm run build errors

---
+++ build/precache-manifest.ba5b1e2dbfc2395e31f8edb59bb8eb6a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b77a71ce49d1bbfac3",
+    "url": "/static/js/main.24b77a71.chunk.js"
+  },
+  {
+    "revision": "240a2ca0f1a3a97a05ea",
+    "url": "/static/js/1.240a2ca0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1121289d41a1d5a268c3c6a5c890d9f1",
+    "url": "/index.html"
+  }
+];
